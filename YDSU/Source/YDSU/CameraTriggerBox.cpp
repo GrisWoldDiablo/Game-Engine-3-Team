596c0872f398c001e79acc556a0001b3c8dfe986
GrisWoldDiablo <griswoldd@hotmail.com>
2019-11-02 00:20:19 -0400

    -* Refactor CameraTriggerBox and MyCharacter cpp
    * removed tag on box and now using cast to determined if MyCharacter is Overlapping with the trigger box instead of the tag.
    - cleaned out obsolete comments and unused includes.
    * refreshed the OnConstruction in the box cpp moved some set to constructor.

--- YDSU/Source/YDSU/CameraTriggerBox.cpp
+++ YDSU/Source/YDSU/CameraTriggerBox.cpp
@@ -2,24 +2,16 @@
 
 
 #include "CameraTriggerBox.h"
-#include "Components/StaticMeshComponent.h"
 #include "Components/ShapeComponent.h"
 #include "Components/TextRenderComponent.h"
 #include "Camera/CameraComponent.h"
-#include "ConstructorHelpers.h"
+//#include "ConstructorHelpers.h"
 
 ACameraTriggerBox::ACameraTriggerBox() {
 
 	//Set default Mesh
 	OriginPoint = CreateDefaultSubobject<USceneComponent>("OriginPoint");
 	OriginPoint->SetupAttachment(RootComponent);
-	//Origin->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	// Find the sphere mesh and assign it
-	/*auto SphereAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/BasicShapes/Sphere.Sphere'"));
-	if (SphereAsset.Object != nullptr)
-	{
-		Origin->SetStaticMesh(SphereAsset.Object);
-	}*/
 	OriginPoint->SetHiddenInGame(true, true);
 
 	// create a camera component
@@ -33,21 +25,19 @@ ACameraTriggerBox::ACameraTriggerBox() {
 	Label->SetHorizontalAlignment(EHorizTextAligment::EHTA_Center);
 	
 	// Set default values to the trigger box.
-	Tags.Add("CamTrigBox");
 	Pitch = -60.0f;
 	Yaw = 0.0f;
 	CameraDistance = 800.0f;
 	DetachCam = false;
 	CamLagSpeed = 1.0f;
 	CamLagDistance = 200.0f;
-
+	CamView->SetRelativeLocation(FVector(CameraDistance * -1.0f, 0.0f, 0.0f));
+	OriginPoint->SetRelativeRotation(FRotator(Pitch, Yaw, 0.0f));
+	Label->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
 }
 
 void ACameraTriggerBox::OnConstruction(const FTransform& Transform)
 {
-	FVector CamViewLoc(CameraDistance * -1.0f, 0.0f, 0.0f);
-	CamView->SetRelativeLocation(CamViewLoc);
-	FRotator originRotation(Pitch, Yaw, 0.0f);
-	OriginPoint->SetRelativeRotation(originRotation);
-	Label->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+	CamView->SetRelativeLocation(FVector(CameraDistance * -1.0f, 0.0f, 0.0f));
+	OriginPoint->SetRelativeRotation(FRotator(Pitch, Yaw, 0.0f));
 }
