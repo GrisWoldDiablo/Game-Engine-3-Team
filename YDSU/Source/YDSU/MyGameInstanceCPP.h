9caf38699c315ea62f2a1f7a69413c2096881109
GrisWoldDiablo <griswoldd@hotmail.com>
2019-12-18 15:46:24 -0500

    + Finisher Camera now CPP and more
    + Added a transition class for the Enemy blueprint, MyEnemyCharacter.cpp, moved all the implementation of the finisher camera to it. and made a blueprint callable function StartFinisherCamera that the blueprints simply call when needed.
    + Added a float curve for the finisher camera.
    + Added a transition class for the GameInstance blueprint MyGameInstanceCPP.cpp moved the CurrentChar variable to it, called it CurrentCharacter and then I retargeted all the blueprints that were using the old variable to the new one.

---
+++ YDSU/Source/YDSU/MyGameInstanceCPP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstanceCPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YDSU_API UMyGameInstanceCPP : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite) class AMyCharacter* CurrentCharacter;
+};
